acc89dd99424b8da317837a4e2a1c4c48111bfb4
Yethum Danith <yethumd@treinetic.com>
2026-07-01 11:26:44 +0530

    auth_gate.dart updated

--- lib/views/auth_gate.dart
+++ lib/views/auth_gate.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:provider/provider.dart';
-
-import '../providers/auth_provider.dart';
 import 'login_screen.dart';
 import 'home_screen.dart';
 
